5f84cb3d99c1f158a3eabbebec48597ca7adf328
Bowske <adabor6@st.amu.edu.pl>
2022-04-28 11:52:53 +0200

    fix: change string

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ function component() {
   const btn = document.createElement("button");
 
   // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(["Hello", "wwikucha"], " ");
+  element.innerHTML = _.join(["Hello", "arek"], " ");
   element.classList.add("hello");
 
   return element;
